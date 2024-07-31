6451c2d1168de0c02abcbe769bab4f54b3ba3d4d
Hernan Provoste <hernanprovostev@outlook.com>
2024-07-31 13:36:26 -0400

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/io/hernan/kmpmovies/App.kt
@@ -0,0 +1,69 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.ImageLoader
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.AsyncImage
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.request.crossfade
+import coil3.util.DebugLogger
+import io.hernan.kmpmovies.Movie
+import io.hernan.kmpmovies.movies
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        setSingletonImageLoaderFactory { context ->
+            ImageLoader.Builder(context)
+                .crossfade(true)
+                .logger(DebugLogger())
+                .build()
+        }
+        Surface(modifier = Modifier.fillMaxSize()) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(120.dp),
+                contentPadding = PaddingValues(4.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                items(movies, key = {it.id}) {
+                    MovieItem(movie = it)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieItem(movie: Movie) {
+    Column {
+        AsyncImage(
+            model = movie.poster,
+            contentDescription = movie.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxHeight().aspectRatio(2/3f).clip(MaterialTheme.shapes.small).background(
+                MaterialTheme.colorScheme.primaryContainer
+            )
+        )
+        Text(
+            text = movie.title,
+            style = MaterialTheme.typography.bodySmall,
+            maxLines = 1,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
